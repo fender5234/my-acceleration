d27a62ce38904fe3e61946ce9f68938809498337
fender5234 <fender5234@gmail.com>
2022-11-30 00:16:42 +0300

    переделал аккордеоны и все работает

--- source/js/modules/modals/accordeon.js
+++ source/js/modules/modals/accordeon.js
@@ -16,17 +16,24 @@ const accordeonToggle = function () {
     element.classList.add('is-hidden-list');
   });
 
-// Тоглл для аккордеонов
+  // Тоглл для аккордеонов
   accordeonSection.forEach((section) => {
     section.addEventListener('click', (event) => {
       accordeonButton.forEach((element) => {
-        const currentElement = element.closest('section');
-        const childList = currentElement.querySelector('ul');
+        const buttons = element;
+        buttons.classList.remove('is-active');
         if (element === event.target) {
+          const currentElement = element.closest('section');
+          const childList = currentElement.querySelector('ul');
+          const currentButton = element;
+          console.log(currentButton);
+          // тут внизу форыч скрывает добавляет класс скрывания списков всех сразу
           accordeonList.forEach((item) => {
             item.classList.add('is-hidden-list');
           });
+          // а тут нужный список который выше был добавлен в переменную открываеться
           childList.classList.remove('is-hidden-list');
+          currentButton.classList.add('is-active');
         }
       });
     });
